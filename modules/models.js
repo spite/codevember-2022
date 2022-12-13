edb444259291998aed724f35c5388da928b9853c
thespite <the.spite@gmail.com>
2022-12-13 00:18:02 +0000

    Create models.js

---
+++ modules/models.js
@@ -0,0 +1,81 @@
+import { GLTFLoader } from "../third_party/GLTFLoader.js";
+import { OBJLoader } from "../third_party/OBJLoader.js";
+import {
+  IcosahedronGeometry,
+  Matrix4,
+  Vector3,
+} from "../third_party/three.module.js";
+import { simplex3 } from "../third_party/perlin.js";
+import { weld, toIndexed } from "../modules/geometry-index.js";
+
+async function loadOBJ(file) {
+  return new Promise((resolve, reject) => {
+    const loader = new OBJLoader();
+    loader.load(file, resolve, null, reject);
+  });
+}
+
+async function loadGLTF(file) {
+  return new Promise((resolve, reject) => {
+    const loader = new GLTFLoader();
+    loader.load(file, resolve, null, reject);
+  });
+}
+
+async function loadSuzanne() {
+  const scene = await loadGLTF("../assets/suzanne.glb");
+  const geometry = scene.scenes[0].children[0].geometry;
+  const mat = new Matrix4().makeRotationX(-Math.PI / 2);
+  geometry.applyMatrix4(mat);
+  return geometry;
+}
+
+async function loadIcosahedron() {
+  const model = await loadOBJ("../assets/icosahedron.obj");
+  const geometry = model.children[0].geometry;
+  geometry.center();
+  const scale = 0.5;
+  geometry.applyMatrix4(new Matrix4().makeScale(scale, scale, scale));
+  return geometry;
+}
+
+async function loadDodecahedron() {
+  const model = await loadOBJ("../assets/dodecahedron.obj");
+  const geometry = model.children[0].geometry;
+  geometry.center();
+  const scale = 0.5;
+  geometry.applyMatrix4(new Matrix4().makeScale(scale, scale, scale));
+  return geometry;
+}
+
+async function loadBox() {
+  const model = await loadOBJ("../assets/box.obj");
+  const geometry = model.children[0].geometry;
+  geometry.center();
+  const scale = 0.5;
+  geometry.applyMatrix4(new Matrix4().makeScale(scale, scale, scale));
+  return geometry;
+}
+
+function generateBlob() {
+  const geometry = weld(toIndexed(new IcosahedronGeometry(1, 30)));
+  const vertices = geometry.attributes.position.array;
+  const v = new Vector3();
+  const scale = 0.05;
+  const noise = 1;
+  for (let i = 0; i < vertices.length; i += 3) {
+    v.set(vertices[i], vertices[i + 1], vertices[i + 2]);
+    v.normalize().multiplyScalar(noise);
+    const n =
+      scale * (simplex3(v.x, v.y, v.z) + simplex3(2 * v.x, 2 * v.y, 2 * v.z));
+    v.multiplyScalar(1 + n);
+    vertices[i] = v.x;
+    vertices[i + 1] = v.y;
+    vertices[i + 2] = v.z;
+  }
+  geometry.scale(0.5, 0.5, 0.5);
+  geometry.computeVertexNormals();
+  return geometry;
+}
+
+export { loadSuzanne, loadIcosahedron, loadBox, generateBlob };
